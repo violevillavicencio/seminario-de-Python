4b0d8a3501fdfd65383c991fc84ed2bd293ca414
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2025-03-20 09:40:31 -0300

    Segunda Funcionalidad
    Cambie el codigo para que al final de la partida se muestre el puntaje del jugador. 
    Si responde incorrectamente se resta 0.5

--- questions.py
+++ questions.py
@@ -28,6 +28,8 @@ answers = [
 correct_answers_index = [1, 2, 0, 3, 1]
 
 # El usuario deberá contestar 3 preguntas
+puntaje = float(0)
+
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
     question_index = random.randint(0, len(questions) - 1)
@@ -38,22 +40,29 @@ for _ in range(3):
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
         user_answer = (input("Respuesta: "))
+        # Si lo ingresado por el usuario no es un digito termina con exit status igual a 1
         if not user_answer.isdigit:
             print ('Respuesta no valida')
             sys.exit(1)  
         user_answer = int(user_answer)
+        # Si lo ingresado por el usuario no esta dentro del rango de preguntas termina con exit status igual a 1
         if not (1 <= user_answer <= 4):
-            print ('Respuesta no entra')
+            print ('Respuesta no valida')
             sys.exit(1)  
         user_answer -= 1
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
+            puntaje += 1
             break
-    else:
-        # Si el usuario no responde correctamente después de 2 intentos,
-        # se muestra la respuesta correcta
-        print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
-    # Se imprime un blanco al final de la pregunta  
-    print()
+        else:
+            # Si el usuario no responde correctamente después de 2 intentos,
+            # se muestra la respuesta correcta
+            print("Incorrecto. La respuesta correcta es:")
+            print(answers[question_index][correct_answers_index[question_index]])
+            # Compruebo que el puntaje no sea 0 asi no me queda un resultado negativo
+            if (puntaje != 0):
+                puntaje -= 0.5
+        # Se imprime un blanco al final de la pregunta  
+        print()
+print(f"Tu puntaje final fue de: {puntaje}")
