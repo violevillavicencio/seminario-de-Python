08f1d1ecb5bb3f39e6b90f35b51339d8ffed8963
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2025-03-20 09:31:09 -0300

    Primera funcionalidad
    Punto uno: agregue dos condiciones para que cuando el usuario ingrese por teclado un valor se chequee si es un digito y luego si este digito cumple con el rango del indice

---
+++ questions.py
@@ -0,0 +1,59 @@
+import sys
+import random
+
+# Preguntas para el juego
+questions = [
+    "¿Qué función se usa para obtener la longitud de una cadena en Python?",
+    "¿Cuál de las siguientes opciones es un número entero en Python?",
+    "¿Cómo se solicita entrada del usuario en Python?",
+    "¿Cuál de las siguientes expresiones es un comentario válido en Python?",
+    "¿Cuál es el operador de comparación para verificar si dos valores son iguales?",
+]
+
+# Respuestas posibles para cada pregunta, en el mismo orden que las preguntas
+answers = [
+    ("size()", "len()", "length()", "count()"),
+    ("3.14", "'42'", "10", "True"),
+    ("input()", "scan()", "read()", "ask()"),
+    (
+    "// Esto es un comentario", 
+    "/* Esto es un comentario */",
+    "-- Esto es un comentario",
+    "# Esto es un comentario",
+    ),
+    ("=", "==", "!=", "==="),
+]
+
+# Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
+correct_answers_index = [1, 2, 0, 3, 1]
+
+# El usuario deberá contestar 3 preguntas
+for _ in range(3):
+    # Se selecciona una pregunta aleatoria
+    question_index = random.randint(0, len(questions) - 1)
+    # Se muestra la pregunta y las respuestas posibles
+    print(questions[question_index])
+    for i, answer in enumerate(answers[question_index]):
+        print(f"{i + 1}. {answer}")
+    # El usuario tiene 2 intentos para responder correctamente
+    for intento in range(2):
+        user_answer = (input("Respuesta: "))
+        if not user_answer.isdigit:
+            print ('Respuesta no valida')
+            sys.exit(1)  
+        user_answer = int(user_answer)
+        if not (1 <= user_answer <= 4):
+            print ('Respuesta no entra')
+            sys.exit(1)  
+        user_answer -= 1
+        # Se verifica si la respuesta es correcta
+        if user_answer == correct_answers_index[question_index]:
+            print("¡Correcto!")
+            break
+    else:
+        # Si el usuario no responde correctamente después de 2 intentos,
+        # se muestra la respuesta correcta
+        print("Incorrecto. La respuesta correcta es:")
+        print(answers[question_index][correct_answers_index[question_index]])
+    # Se imprime un blanco al final de la pregunta  
+    print()
